152af3fbb913d710c690ed2b65f44325245b0f1e
snehapaatil24-ui <snehapaatil24@gmail.com>
2026-06-02 13:05:16 +0530

    first push project

---
+++ Newproject/src/test/java/pac1/ContactTest.java
@@ -0,0 +1,26 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createcontactTest() {
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser", "CHROME");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		System.out.println("execute createcontactTest");
+
+	}
+
+	@Test
+	public void modifycontactTest() {
+		System.out.println("execute modifycontactTest");
+
+	}
+
+}
---
+++ Newproject/src/test/java/pac2/OrgTest.java
@@ -0,0 +1,15 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	
+	@Test
+	public void createorgtest() {
+		System.out.println("execute createorgtest ");
+	}
+	@Test
+	public void modifyorgtest() {
+		System.out.println("execute modifyorgtest ");
+	}
+}
